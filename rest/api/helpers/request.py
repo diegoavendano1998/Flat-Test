942c28b42643f63bdffe3282ad883dfd50d18e14
Diego AB <diegoavendano1998@gmail.com>
2022-06-16 01:06:15 -0500

    Implement responseJSON method

---
+++ rest/api/helpers/request.py
@@ -0,0 +1,19 @@
+import json
+
+
+# Send custom JSON response
+def responseJSON(data, message, code):
+    if code != 200:
+        return json.dumps(
+            {
+                'code': code,
+                'message': message
+            }
+        ), 200
+    else:
+        return json.dumps(
+            {
+                'code': code,
+                'data': data
+            }
+        ), 200
